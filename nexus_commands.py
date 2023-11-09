3ea2652c061ba5eaa78346aa3d16d03e8bab8339
TheHidN <g.hung18@gmail.com>
2023-11-09 22:52:17 +0000

    1.10

--- nexus_commands.py
+++
@@ -1,7 +0,0 @@
-def nexus_mac():
-    from ssh import login
-    session = login()
-    session.expect('#')
-    mac_address_table = session.sendline('show mac address-table')
-
-    return session
--- ssh.py
+++ ssh.py
@@ -1,42 +1,52 @@
 from getpass import getpass
 from netmiko import ConnectHandler
-from openpyxl import Workbook
+import json
 import time
 
 username = input('login as: ')
 password = getpass('Password: ')
+devicetype = input('Type 1 for Nexus Switch, 2 for XE switch: ')
 
-commands = ['show mac address-table','show ip arp','show interface status',
-        'show port-channel sum', 'show vpc | inc up']
+if devicetype == '1':
+    devicetype = 'cisco_nxos'
+elif devicetype == '2':
+    devicetype = 'cisco_ios'
+else:
+    print('Enter a valid number')
+
+creds = {
+    'port': '22',
+    'username': username,
+    'password': password,
+    'device_type': devicetype
+}
+
+nex_command = ['show mac address-table']
+#nex_command = ['show mac address-table','show ip arp','show interface status',
+      #  'show port-channel sum', 'show vpc | inc up']
+
+ios_command = []
+
+json_list = []
 
 def login(ip):
+    #connect to switch
+    session = ConnectHandler(host=ip, **creds)
     
-    session = ConnectHandler(host=ip,
-                             port='22',
-                             username=username,
-                             password=password,
-                             device_type='cisco_ios')
+    #send command output as json to convert to excel.
+    if devicetype =='cisco_nxos':
+        
+        for cmd in nex_command:
+            output = session.send_command(cmd, use_textfsm=True)
+            
+            with open(f'{cmd}.json', 'w') as f:
+                json.dump(output, f, indent=2)
 
-    workbook = Workbook()
-    workbook.active
-    
-    #loops commands through cisco cli and outputs line by line to excel.
-    for cmd in commands:
-        worksheet = workbook.create_sheet(title=cmd.rstrip())
-        output = session.send_command(cmd).split()
-        for i in range(0, len(output)):
-            start_row = i + 1
-            e = worksheet.cell(row=start_row, column=1)
-            e.value = output[i]
-    
-    #delete sheet1
-    sheet1 = workbook.get_sheet_by_name('Sheet')
-    workbook.remove_sheet(sheet1)
-
-    workbook.save(filename='test.xlsx')
+    elif devicetype =='cisco_ios':
+        for cmd in ios_commands:
+            output = session.send_command(ios_commands, use_textfsm=True)
+            pprint(output[0])
 
     session.disconnect()
-
-    print(output[0])
 
 login('172.21.98.34')
