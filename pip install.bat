d94c385eb8d5ad20ff77187f401da484ac9e4f6e
TheHidN <g.hung18@gmail.com>
2023-11-09 22:42:14 -0500

    2.00

---
+++ pip install.bat
@@ -0,0 +1,5 @@
+pip install netmiko
+pip install openpyxl
+pip install pandas
+pip install json
+pip install termcolor
--- ssh.py
+++ ssh.py
@@ -1,16 +1,18 @@
 from getpass import getpass
-from netmiko import ConnectHandler
 import json
-import time
+from netmiko import ConnectHandler
+import os
+import openpyxl
+import pandas as pd
 
 username = input('login as: ')
 password = getpass('Password: ')
-devicetype = input('Type 1 for Nexus Switch, 2 for XE switch: ')
+device_type = input('Type 1 for IOS, 2 for Nexus: ')
 
-if devicetype == '1':
-    devicetype = 'cisco_nxos'
-elif devicetype == '2':
+if device_type == '1':
     devicetype = 'cisco_ios'
+elif device_type == '2':
+    devicetype = 'cisco_nxos'
 else:
     print('Enter a valid number')
 
@@ -21,32 +23,77 @@ creds = {
     'device_type': devicetype
 }
 
-nex_command = ['show mac address-table']
-#nex_command = ['show mac address-table','show ip arp','show interface status',
-      #  'show port-channel sum', 'show vpc | inc up']
+ios_command = [
+    'show clock',
+    'show ver',
+    'show mac address-table',
+    'show ip arp',
+    'show interface status',
+    'show cdp neighbors',
+    'sh ip int br',
+    'sh ip int br | inc up',
+    'sh ip int br | inc down',
+    'sh ip int br | inc admin'
+]
 
-ios_command = []
-
-json_list = []
+nex_command = ['show mac address-table',
+               'show ip arp',
+               'show interface status',
+               'show port-channel sum',
+               'show vpc | inc up'
+]
 
 def login(ip):
     #connect to switch
     session = ConnectHandler(host=ip, **creds)
-    
-    #send command output as json to convert to excel.
-    if devicetype =='cisco_nxos':
-        
-        for cmd in nex_command:
-            output = session.send_command(cmd, use_textfsm=True)
-            
-            with open(f'{cmd}.json', 'w') as f:
-                json.dump(output, f, indent=2)
 
-    elif devicetype =='cisco_ios':
-        for cmd in ios_commands:
-            output = session.send_command(ios_commands, use_textfsm=True)
-            pprint(output[0])
+    #get hostname
+    hostname = session.find_prompt()[:-1]
+
+    #create excel
+    wb = openpyxl.Workbook()
+    wb.save(hostname + ".xlsx")
+
+    #send command output as json to convert to excel.
+    if devicetype =='cisco_ios':
+        for cmd in ios_command:
+
+            with pd.ExcelWriter(hostname +".xlsx", engine="openpyxl", mode="a", if_sheet_exists="overlay") as writer:
+                output = session.send_command(cmd, use_textfsm=True)
+                router_output = json.dumps(output, indent=4)
+                convert_json = json.loads(router_output)
+                try:
+                    df = pd.json_normalize(convert_json)
+                    print(f'Grabbing configs from: {hostname}, {ip} Command: {cmd}')
+                    df.to_excel(writer, sheet_name=(cmd), index=False)
+                except:
+                    print(f'Unable to grab: {hostname}, {ip} Command: {cmd}')
+                    with open ('error.txt', 'a') as e:
+                        e.write(f'Unable to grab: {hostname}, {ip} Command: {cmd}')
+
+    elif devicetype =='cisco_nxos':
+        for cmd in nex_command:
+
+            with pd.ExcelWriter(hostname +".xlsx", engine="openpyxl", mode="a", if_sheet_exists="overlay") as writer:
+                output = session.send_command(cmd, use_textfsm=True)
+                router_output = json.dumps(output, indent=4)
+                convert_json = json.loads(router_output)
+                try:
+                    df = pd.json_normalize(convert_json)
+                    print(f'Grabbing configs from: {hostname}, {ip} Command: {cmd}')
+                    df.to_excel(writer, sheet_name=(cmd), index=False)
+                except:
+                    print(f'Unable to grab: {hostname}, {ip} Command: {cmd}')
+                    with open ('error.txt', 'a') as e:
+                        e.write(f'Unable to grab: {hostname}, {ip} Command: {cmd}')
+
+    wb = openpyxl.load_workbook(hostname +".xlsx")
+    del wb['Sheet']
+    wb.save(hostname +".xlsx")
 
     session.disconnect()
 
-login('172.21.98.34')
+with open('ip.txt', 'r') as ip_list:
+    for ip in ip_list:
+        login(ip)
+
